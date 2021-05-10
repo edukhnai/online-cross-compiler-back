3a9a77cca831db89484e2a70bc5c401945e0eeda
Ekaterina Dukhnay <e.dukhnay@tinkoff.ru>
2021-05-10 15:39:25 +0300

    Add CORS support, rename DTO properties' names

--- src/main/kotlin/com/katedukhnai/online/cross/compiler/back/OnlineCrossCompilerBackApplication.kt
+++ src/main/kotlin/com/katedukhnai/online/cross/compiler/back/OnlineCrossCompilerBackApplication.kt
@@ -2,9 +2,25 @@ package com.katedukhnai.online.cross.compiler.back
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
 
 @SpringBootApplication
-class OnlineCrossCompilerBackApplication
+class OnlineCrossCompilerBackApplication {
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer? {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/compile").allowedOrigins("http://localhost:4200")
+                registry.addMapping("/compile-execute").allowedOrigins("http://localhost:4200")
+            }
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<OnlineCrossCompilerBackApplication>(*args)
